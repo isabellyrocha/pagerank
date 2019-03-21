4216751047a9dd396896ec159e8e1843dce94c81
Isabelly Rocha <isabellylr@gmail.com>
2019-03-21 11:49:14 +0100

    Creating deployment class.

--- deploy.py
+++ deploy.py
@@ -11,7 +11,7 @@ from metrics.kubernetes import Kubernetes
 def main():
     k8s = Kubernetes() 
     number_of_pods = 10
-    for pod_id in range(number_of_pod):
+    for pod_id in range(number_of_pods):
         pod = k8s.create_pagerank_pod('pagerank-seq-'+pod_id, 'vully-1')
         k8s.deploy_pod(pod)
         while (not k8s.is_finished()):
