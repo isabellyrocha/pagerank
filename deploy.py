a2d8ff73543e0958cb4039d7439c9a3928213605
Isabelly Rocha <isabellylr@gmail.com>
2019-03-21 17:18:17 +0100

    deploy

--- deploy.py
+++ deploy.py
@@ -11,7 +11,7 @@ from metrics.kubernetes import Kubernetes
 def main():
     k8s = Kubernetes() 
     number_of_pods = 10
-    for pod_id in range(number_of_pods+1,number_of_pods+2):
+    for pod_id in range(number_of_pods):
         pod_name = 'pagerank-seq-%d' % (pod_id)
         k8s.deploy_pod(k8s.create_pagerank_pod(pod_name, 'vully-1'))
         time.sleep(30)
@@ -19,6 +19,19 @@ def main():
         while (not k8s.is_finished(pod)):
             time.sleep(30)
             pod = k8s.get_pod(pod_name)
+            
+    for pod_id in range(number_of_pods):
+        pod_name_1 = 'pagerank-dist-1-%d' % (pod_id)
+        pod_name_2 = 'pagerank-dist-2-%d' % (pod_id)
+        k8s.deploy_pod(k8s.create_pagerank_pod(pod_name_1, 'vully-1'))
+        k8s.deploy_pod(k8s.create_pagerank_pod(pod_name_2, 'vully-2'))
+        time.sleep(30)
+        pod_1 = k8s.get_pod(pod_name_1)
+        pod_2 = k8s.get_pod(pod_name_2)
+        while (not (k8s.is_finished(pod_1) and k8s.is_finished(pod_2))):
+            time.sleep(30)
+            pod_1 = k8s.get_pod(pod_name_1)
+            pod_2 = k8s.get_pod(pod_name_1)
 
 if __name__ == '__main__':
     main()
